87e410acdcf9daea2674980cdece6270cdf316d3
Marion <contact@marioncasters.fr>
2025-01-23 08:52:21 +0100

    Adding category ans year filtering feature for admin

--- src/app/admin/dessins/page.tsx
+++ src/app/admin/dessins/page.tsx
@@ -3,7 +3,10 @@ import ItemListComponent from "@/components/admin/item/ItemListComponent";
 import CategoryComponent from "@/components/admin/item/category/CategoryComponent";
 import s from "@/styles/admin/Admin.module.css";
 import React from "react";
-import { getDrawingsFull } from "@/app/api/dessin/getDrawings";
+import {
+  getDrawingsFull,
+  getYearsForDrawing,
+} from "@/app/api/dessin/getDrawings";
 import { getDrawingCategoriesFull } from "@/app/api/dessin/category/getCategories";
 import ItemForm from "@/components/admin/form/ItemForm";
 import { getEmptyItem } from "@/utils/commonUtils";
@@ -11,11 +14,16 @@ import { getEmptyItem } from "@/utils/commonUtils";
 export default async function Dessins() {
   const drawings = await getDrawingsFull();
   const categories = await getDrawingCategoriesFull();
+  const years = await getYearsForDrawing();
 
   return (
     <>
       <h1 className={s.pageTitle}>Contenus des pages Dessins</h1>
-      <ItemListComponent items={drawings} categories={categories} />
+      <ItemListComponent
+        items={drawings}
+        categories={categories}
+        years={years}
+      />
       <ItemForm categories={categories} item={getEmptyItem(Type.DRAWING)} />
       <CategoryComponent itemType={Type.DRAWING} categories={categories} />
     </>
