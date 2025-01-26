9bbae92994850d5764f45f2a3e0f6dd7b049f3ae
Marion <contact@marioncasters.fr>
2025-01-26 01:14:17 +0100

    Delete category drawing api

--- src/app/admin/api/dessin/category/route.ts
+++
@@ -1,6 +0,0 @@
-import { getDrawingCategoriesFull } from "@/app/actions/drawings";
-
-export async function GET() {
-  const res = await getDrawingCategoriesFull();
-  return Response.json({ res }, { status: 200 });
-}
--- src/app/admin/api/dessin/update/route.ts
+++
@@ -1,8 +0,0 @@
-export async function POST(req: Request) {
-  try {
-    return Response.json({ message: "ok" }, { status: 200 });
-  } catch (e) {
-    console.log(e);
-    return Response.json({ error: "Error" }, { status: 404 });
-  }
-}
