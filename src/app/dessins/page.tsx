a6e4eb63f4b37b5a96faf5cb0a6a5bca0b4e506a
Marion <contact@marioncasters.fr>
2025-02-04 10:14:30 +0100

    Feat: home item - drawing path

--- src/app/dessins/page.tsx
+++ src/app/dessins/page.tsx
@@ -1,14 +1,21 @@
 import ItemComponent from "@/components/item/ItemComponent";
 import { ItemFull } from "@/lib/type";
 
-import { getDrawingsFull } from "@/app/actions/drawings";
+import {
+  getDrawingCategoriesFull,
+  getDrawingsFull,
+} from "@/app/actions/drawings";
+import ItemPageComponent from "@/components/item/ItemPageComponent";
+import { TAGS } from "@/constants/specific/routes";
 
 export default async function Page() {
   const drawings = await getDrawingsFull();
-  return (
-    drawings.length > 0 &&
-    drawings.map((drawing: ItemFull) => (
+  const categories = await getDrawingCategoriesFull();
+
+  if (categories.length > 0)
+    return <ItemPageComponent categories={categories} tag={TAGS.DRAWING} />;
+  else
+    return drawings.map((drawing: ItemFull) => (
       <ItemComponent key={drawing.id} item={drawing} />
-    ))
-  );
+    ));
 }
--- src/app/dessins/par_annee/page.tsx
+++ src/app/dessins/par_annee/page.tsx
@@ -1,10 +1,15 @@
 import ItemYearComponent from "@/components/item/ItemYearComponent";
 
 import { getDrawingsFull, getYearsForDrawing } from "@/app/actions/drawings";
+import s from "@/styles/ItemPage.module.css";
 
 export default async function Page() {
   const drawings = await getDrawingsFull();
   const years = await getYearsForDrawing();
 
-  return <ItemYearComponent items={drawings} years={years} />;
+  return (
+    <div className={s.paintingContent}>
+      <ItemYearComponent items={drawings} years={years} />
+    </div>
+  );
 }
