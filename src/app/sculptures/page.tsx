9a07b213c457735b3e65d356031211825907af11
Marion <contact@marioncasters.fr>
2025-02-04 10:17:04 +0100

    Feat: home item - sculpture path

--- src/app/sculptures/page.tsx
+++ src/app/sculptures/page.tsx
@@ -1,14 +1,18 @@
 import ItemComponent from "@/components/item/ItemComponent";
 import { ItemFull } from "@/lib/type";
 import { getSculpturesFull } from "@/app/actions/sculptures";
+import { getPaintingCategoriesFull } from "@/app/actions/paintings";
+import ItemPageComponent from "@/components/item/ItemPageComponent";
+import { TAGS } from "@/constants/specific/routes";
 
 export default async function Page() {
   const sculptures: ItemFull[] = await getSculpturesFull();
+  const categories = await getPaintingCategoriesFull();
 
-  return (
-    sculptures.length > 0 &&
-    sculptures.map((sculpture: ItemFull) => (
+  if (categories.length > 0)
+    return <ItemPageComponent categories={categories} tag={TAGS.SCULPTURE} />;
+  else
+    return sculptures.map((sculpture: ItemFull) => (
       <ItemComponent key={sculpture.id} item={sculpture} />
-    ))
-  );
+    ));
 }
--- src/app/sculptures/par_annee/page.tsx
+++ src/app/sculptures/par_annee/page.tsx
@@ -4,10 +4,15 @@ import {
   getSculpturesFull,
   getYearsForSculpture,
 } from "@/app/actions/sculptures";
+import s from "@/styles/ItemPage.module.css";
 
 export default async function Page() {
   const sculptures = await getSculpturesFull();
   const years = await getYearsForSculpture();
 
-  return <ItemYearComponent items={sculptures} years={years} />;
+  return (
+    <div className={s.sculptureContent}>
+      <ItemYearComponent items={sculptures} years={years} />
+    </div>
+  );
 }
