ffc665d84d162a4c319fb3405fcf45f157f3d3f2
Marion <contact@marioncasters.fr>
2025-03-01 17:22:02 +0100

    Fix bug on build

--- src/constants/specific/index.js
+++ src/constants/specific/index.js
@@ -10,35 +10,54 @@ export const META = {
 export const BASE_THEME = {
   name: THEME.BASE_THEME,
   isActive: true,
+
+  // general
   lineColor: "#be2d01",
   backgroundColor: "#fafafa",
-  backgroundColorItem: "#fafafa",
-  color: "#333",
-  colorItem: "#333",
   titleColor: "#333",
-
+  color: "#333",
   linkColor: "#be2d01",
   linkHoverColor: "#ee7845",
-  linkItemColor: "#be2d01",
-  linkHoverItemColor: "#be2d01",
 
+  // item
+  backgroundColorItem: "#fafafa",
+  colorItem: "#333",
+  linkItemColor: "#be2d01",
+  linkHoverItemColor: "#ee7845",
+
+  /*
+   menu 1
+   */
+  // general
   menu1Color: "#e7e7e7",
-  menu1HomeColor: "#e7e7e7",
-  menu1ItemColor: "#e7e7e7",
   menu1LinkColor: "#333",
   menu1LinkHoverColor: "#be2d01",
+
+  // home
+  menu1HomeColor: "#e7e7e7",
   menu1LinkHomeColor: "#333",
   menu1LinkHomeHoverColor: "#be2d01",
+
+  // item
+  menu1ItemColor: "#e7e7e7",
   menu1LinkItemColor: "#333",
   menu1LinkHoverItemColor: "#be2d01",
 
+  /*
+   menu 2
+   */
+  // general
   menu2Color: "#fafafa",
-  menu2HomeColor: "#fafafa",
-  menu2ItemColor: "#fafafa",
   menu2LinkColor: "#be2d01",
   menu2LinkHoverColor: "#333",
+
+  // home
+  menu2HomeColor: "#fafafa",
   menu2LinkHomeColor: "#be2d01",
   menu2LinkHomeHoverColor: "#333",
+
+  // item
+  menu2ItemColor: "#fafafa",
   menu2LinkItemColor: "#be2d01",
   menu2LinkHoverItemColor: "#333",
 };
