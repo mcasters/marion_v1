21c3764f6b4244b30ce8c7f4b3ab80504305facc
Marion <contact@marioncasters.fr>
2025-03-12 13:40:37 +0100

    Using metas context admin form

--- src/app/dessins/par-annee/[year]/page.tsx
+++ src/app/dessins/par-annee/[year]/page.tsx
@@ -3,7 +3,7 @@ import ItemsComponent from "@/components/item/ItemsComponent";
 import { Type } from "@/lib/type";
 import { getSession } from "@/app/lib/auth";
 import { Metadata } from "next";
-import { getItemLayout, getMetaMap } from "@/utils/commonUtils";
+import { getMetaMap } from "@/utils/commonUtils";
 import { getMetas } from "@/app/actions/meta";
 import { META } from "@/constants/specific";
 
@@ -11,11 +11,10 @@ type Props = {
   params: Promise<{ year: string }>;
 };
 
-const metas = getMetaMap(await getMetas());
-
 export async function generateMetadata({
   params,
 }: Props): Promise<Metadata | undefined> {
+  const metas = getMetaMap(await getMetas());
   const year = (await params).year;
   if (metas) {
     return {
@@ -38,11 +37,5 @@ export default async function Page({ params }: Props) {
   const session = await getSession();
   const items = await getItemsByYear(year, Type.DRAWING, !session);
 
-  return (
-    <ItemsComponent
-      tag={year}
-      items={items}
-      layout={getItemLayout(metas.get(META.DRAWING_LAYOUT))}
-    />
-  );
+  return <ItemsComponent tag={year} items={items} type={Type.DRAWING} />;
 }
