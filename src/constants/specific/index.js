8628a51dea5bc619e641397fc99b5be699e3e66c
Marion <contact@marioncasters.fr>
2025-03-18 00:20:25 +0100

    BaseTheme datas

--- src/constants/specific/index.js
+++ src/constants/specific/index.js
@@ -1,58 +1,58 @@
-import { THEME, THEME_PROPS } from "../admin";
+import { THEME } from "../admin";
 
 export const BASE_THEME = {
   name: THEME.BASE_THEME,
   isActive: true,
 
   // general
-  [THEME_PROPS.LINE_COLOR]: "#be2d01",
-  [THEME_PROPS.BD_COLOR]: "#fafafa",
-  [THEME_PROPS.TITLE_COLOR]: "#333",
-  [THEME_PROPS.COLOR]: "#333",
-  [THEME_PROPS.LINK_COLOR]: "#be2d01",
-  [THEME_PROPS.LINK_HOVER_COLOR]: "#ee7845",
+  lineColor: "#be2d01",
+  backgroundColor: "#fafafa",
+  titleColor: "#333",
+  color: "#333",
+  linkColor: "#be2d01",
+  linkHoverColor: "#ee7845",
 
   // item
-  [THEME_PROPS.BD_COLOR_ITEM]: "#fafafa",
-  [THEME_PROPS.COLOR_ITEM]: "#333",
-  [THEME_PROPS.LINK_ITEM_COLOR]: "#be2d01",
-  [THEME_PROPS.LINK_HOVER_ITEM_COLOR]: "#ee7845",
+  backgroundColorItem: "#fafafa",
+  colorItem: "#333",
+  linkItemColor: "#be2d01",
+  linkHoverItemColor: "#ee7845",
 
   /*
    menu 1
    */
   // general
-  [THEME_PROPS.MENU_1_COLOR]: "#e7e7e7",
-  [THEME_PROPS.MENU_1_LINK_COLOR]: "#333",
-  [THEME_PROPS.MENU_1_LINK_HOVER_COLOR]: "#be2d01",
+  menu1Color: "#e7e7e7",
+  menu1LinkColor: "#333",
+  menu1LinkHoverColor: "#be2d01",
 
   // home
-  [THEME_PROPS.MENU_1_HOME_COLOR]: "#e7e7e7",
-  [THEME_PROPS.MENU_1_LINK_HOME_COLOR]: "#333",
-  [THEME_PROPS.MENU_1_LINK_HOVER_HOME_COLOR]: "#be2d01",
+  menu1HomeColor: "#e7e7e7",
+  menu1LinkHomeColor: "#333",
+  menu1LinkHomeHoverColor: "#be2d01",
 
   // item
-  [THEME_PROPS.MENU_1_ITEM_COLOR]: "#e7e7e7",
-  [THEME_PROPS.MENU_1_LINK_ITEM_COLOR]: "#333",
-  [THEME_PROPS.MENU_1_LINK_HOVER_ITEM_COLOR]: "#be2d01",
+  menu1ItemColor: "#e7e7e7",
+  menu1LinkItemColor: "#333",
+  menu1LinkHoverItemColor: "#be2d01",
 
   /*
    menu 2
    */
   // general
-  [THEME_PROPS.MENU_2_COLOR]: "#fafafa",
-  [THEME_PROPS.MENU_2_LINK_COLOR]: "#be2d01",
-  [THEME_PROPS.MENU_2_LINK_HOVER_COLOR]: "#333",
+  menu2Color: "#fafafa",
+  menu2LinkColor: "#be2d01",
+  menu2LinkHoverColor: "#333",
 
   // home
-  [THEME_PROPS.MENU_2_HOME_COLOR]: "#fafafa",
-  [THEME_PROPS.MENU_2_LINK_HOME_COLOR]: "#be2d01",
-  [THEME_PROPS.MENU_2_LINK_HOVER_HOME_COLOR]: "#333",
+  menu2HomeColor: "#fafafa",
+  menu2LinkHomeColor: "#be2d01",
+  menu2LinkHomeHoverColor: "#333",
 
   // item
-  [THEME_PROPS.MENU_2_ITEM_COLOR]: "#fafafa",
-  [THEME_PROPS.MENU_2_LINK_ITEM_COLOR]: "#be2d01",
-  [THEME_PROPS.MENU_2_LINK_HOVER_ITEM_COLOR]: "#333",
+  menu2ItemColor: "#fafafa",
+  menu2LinkItemColor: "#be2d01",
+  menu2LinkHoverItemColor: "#333",
 };
 
 export const BASE_PRESET_COLOR = {
