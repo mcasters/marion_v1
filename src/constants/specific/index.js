0e10442c1fd4b6ab9665dbda986f0e838f0ac4f9
Marion <contact@marioncasters.fr>
2025-03-28 12:35:58 +0100

    Theme utils

--- src/constants/specific/index.js
+++ src/constants/specific/index.js
@@ -7,11 +7,15 @@ export const BASE_THEME = {
   // general
   lineColor: "#be2d01",
   backgroundColor: "#fafafa",
-  titleColor: "#333",
-  introColor: "#333",
   color: "#333",
   linkColor: "#be2d01",
   linkHoverColor: "#ee7845",
+
+  // home
+  titleColor: "#333",
+  introColor: "#333",
+  footerBackgroundColorHome: "#be2d01",
+  footerColorHome: "#ee7845",
 
   // item
   backgroundColorItem: "#fafafa",
@@ -54,6 +58,67 @@ export const BASE_THEME = {
   menu2ItemColor: "#fafafa",
   menu2LinkItemColor: "#be2d01",
   menu2LinkHoverItemColor: "#333",
+};
+
+export const THEME_NEW = {
+  name: THEME.BASE_THEME,
+  isActive: true,
+  lineColor_gen: "#be2d01",
+  titleColor_gen: "#333",
+
+  // homePage
+  menu1_background_home: "#e7e7e7",
+  menu1_link_home: "#333",
+  menu1_linkHover_home: "#be2d01",
+
+  menu2_background_home: "#fafafa",
+  menu2_link_home: "#be2d01",
+  menu2_linkHover_home: "#333",
+
+  main_text_home: "#333",
+
+  footer_background_home: "#be2d01",
+  footer_text_home: "#ee7845",
+  footer_link_home: "#ee7845",
+  footer_linkHover_home: "#f5a786",
+
+  // item
+  menu1_background_item: "#e7e7e7",
+  menu1_link_item: "#333",
+  menu1_linkHover_item: "#be2d01",
+
+  menu2_background_item: "#fafafa",
+  menu2_link_item: "#be2d01",
+  menu2_linkHover_item: "#333",
+
+  main_background_item: "#fafafa",
+  main_text_item: "#333",
+  main_link_item: "#be2d01",
+  main_linkHover_item: "#ee7845",
+
+  footer_background_item: "#be2d01",
+  footer_text_item: "#ee7845",
+  footer_link_item: "#ee7845",
+  footer_linkHover_item: "#f5a786",
+
+  // other
+  menu1_background_other: "#e7e7e7",
+  menu1_link_other: "#333",
+  menu1_linkHover_other: "#be2d01",
+
+  menu2_background_other: "#fafafa",
+  menu2_link_other: "#be2d01",
+  menu2_linkHover_other: "#333",
+
+  main_background_other: "#fafafa",
+  main_text_other: "#333",
+  main_link_other: "#be2d01",
+  main_linkHover_other: "#ee7845",
+
+  footer_background_other: "#fafafa",
+  footer_text_other: "#333",
+  footer_link_other: "#be2d01",
+  footer_linkHover_other: "#ee7845",
 };
 
 export const BASE_PRESET_COLOR = {
