2e06cb6a2b3ea1abd8ed641f59dc58a3ddfd6bee
Marion <contact@marioncasters.fr>
2025-04-09 12:56:14 +0200

    Remove old category component

--- src/components/admin/item/category/categoryComponent.tsx
+++ src/components/admin/item/category/categoryComponent.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import CategoryListComponent from "@/components/admin/item/category/categoryListComponent";
-import CategoryForm from "@/components/admin/form/category/categoryForm";
-import s from "@/components/admin/admin.module.css";
+import RowCategoryListComponent from "./rowCategoryListComponent";
+import React from "react";
+import s from "@/components/admin/adminList.module.css";
+import style from "@/components/admin/admin.module.css";
 import { Category, ItemFull, Type } from "@/lib/type";
+import AddUpdateCategoryButton from "@/components/admin/form/category/addUpdateCategoryButton";
 import { getEmptyCategory } from "@/utils/commonUtils";
 
 interface Props {
@@ -11,18 +13,35 @@ interface Props {
   categories: Category[];
   items: ItemFull[];
 }
-export default function CategoryComponent({ categories, type, items }: Props) {
-  const title = "Gestion des catégories";
+export default function CategoryComponent({ type, categories, items }: Props) {
+  const message = `Une catégorie ne peut être supprimée que lorsqu'il n'y a pas ou plus de ${type} qui y est classée.`;
 
   return (
-    <div className={s.container}>
-      <h2 className={s.title2}>{title}</h2>
-      <CategoryListComponent
-        type={type}
-        categories={categories}
+    <div className={style.container}>
+      <h2 className={style.title2}>Gestion des catégories</h2>
+      <div className={`${s.categoryListWrapper} area`}>
+        {categories.map((category) => {
+          let itemTab;
+          if (category.key === "no-category")
+            itemTab = items.filter((item) => item.categoryId === null);
+          else
+            itemTab = items.filter((item) => item.categoryId === category.id);
+          return (
+            <RowCategoryListComponent
+              key={category.id}
+              category={category}
+              items={itemTab}
+              type={type}
+            />
+          );
+        })}
+      </div>
+      <h5>{message}</h5>
+      <AddUpdateCategoryButton
+        category={getEmptyCategory()}
         items={items}
+        type={type}
       />
-      <CategoryForm category={getEmptyCategory()} type={type} items={items} />
     </div>
   );
 }
