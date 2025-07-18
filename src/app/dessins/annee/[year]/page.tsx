4d1436fd3a96e7e2678cb5f529dac7b173752038
Marion <contact@marioncasters.fr>
2025-07-18 09:02:35 +0200

    Changing route year

--- src/app/dessins/annee/[year]/page.tsx
+++ src/app/dessins/annee/[year]/page.tsx
@@ -1,8 +1,8 @@
 import ItemsPageComponent from "@/components/item/itemsPageComponent";
 import { Type } from "@/lib/type";
+import { Metadata } from "next";
 import { getMetaMap } from "@/utils/commonUtils";
 import { getMetas } from "@/app/actions/meta";
-import { Metadata } from "next";
 import { META } from "@/constants/admin";
 import { getItemsByYear } from "@/app/actions/item-post";
 
@@ -13,15 +13,15 @@ type Props = {
 export async function generateMetadata({
   params,
 }: Props): Promise<Metadata | undefined> {
-  const year = (await params).year;
   const metas = getMetaMap(await getMetas());
+  const { year } = await params;
   if (metas) {
     return {
-      title: `${metas.get(META.DOCUMENT_TITLE_PAINTING)} - Année ${year}`,
-      description: `${metas.get(META.DESCRIPTION_PAINTING)} - Année ${year}`,
+      title: `${metas.get(META.DOCUMENT_TITLE_DRAWING)} - Année ${year}`,
+      description: `${metas.get(META.DESCRIPTION_DRAWING)} - Année ${year}`,
       openGraph: {
-        title: `${metas.get(META.DOCUMENT_TITLE_PAINTING)} - Année ${year}`,
-        description: `${metas.get(META.DESCRIPTION_PAINTING)} - Année ${year}`,
+        title: `${metas.get(META.DOCUMENT_TITLE_DRAWING)} - Année ${year}`,
+        description: `${metas.get(META.DESCRIPTION_DRAWING)} - Année ${year}`,
         url: metas.get(META.URL),
         siteName: metas.get(META.SEO_SITE_TITLE),
         locale: "fr",
@@ -32,8 +32,8 @@ export async function generateMetadata({
 }
 
 export default async function Page({ params }: Props) {
-  const year = (await params).year;
-  const items = await getItemsByYear(year, Type.PAINTING);
+  const { year } = await params;
+  const items = await getItemsByYear(year, Type.DRAWING);
 
-  return <ItemsPageComponent tag={year} items={items} type={Type.PAINTING} />;
+  return <ItemsPageComponent tag={year} items={items} type={Type.DRAWING} />;
 }
